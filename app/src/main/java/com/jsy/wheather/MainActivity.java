7c40ace11b2ccc2674b3ed84c21b231f9239d0a3
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-25 21:45:28 +0900

    값 들고와서 시간별로 뿌려주기 가능, 프론트 조금 수정

--- app/src/main/java/com/jsy/wheather/MainActivity.java
+++ app/src/main/java/com/jsy/wheather/MainActivity.java
@@ -1,59 +1,56 @@
 package com.jsy.wheather;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.JsonToken;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.channels.InterruptedByTimeoutException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 100;
 
     ArrayList<String> list;
-    TextView tvdeg,tvcom1;
-    Button btndeg,btncome,btnvlist;
-    SQLiteDatabase sqlDB;
-    myDBHelper myHelper;
-    Date date;
+    TextView tvdeg,tvcom1,tvcloth,tvupdate;
+    Button btncome,btnvlist;
+    SQLiteDatabase sqlDB,sqlDB2;
+    myDBHelper myHelper,myHelper2;
+    Date date,date1;
     String locnx;
     String locny;
 
     private TextView tvloc;
+
+    AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,13 +60,17 @@ public class MainActivity extends AppCompatActivity {
 
         tvdeg = (TextView)findViewById(R.id.tvdeg);
         tvcom1 = (TextView)findViewById(R.id.tvcom1);
+        tvcloth = (TextView)findViewById(R.id.tvcloth);
+        tvupdate = (TextView)findViewById(R.id.tvupdate);
 
-        btndeg = (Button)findViewById(R.id.btndeg);
         btncome = (Button)findViewById(R.id.btncome);
         btnvlist = (Button)findViewById(R.id.btnvlist);
+
         list = new ArrayList<>();
         myHelper = new myDBHelper(this);
+        myHelper2 = new myDBHelper(this);
         date = new Date(System.currentTimeMillis());
+        date1 = new Date(System.currentTimeMillis());
 
         tvloc = (TextView)findViewById(R.id.tvloc);
         //지역선택 버튼 눌렀을 때 ↓
@@ -90,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
             //인터넷 연결을 위한 쓰레드↓
             @Override
             public void onClick(View v) {
+                tvupdate.setText("새로고침 중입니다...(약20초 소요)");
                 sqlDB = myHelper.getWritableDatabase();
                 myHelper.onUpgrade(sqlDB,1,2);
                 Thread t = new Thread(new Runnable() {
@@ -98,12 +100,21 @@ public class MainActivity extends AppCompatActivity {
 
                         try {
                             weather();
+                            Log.i("loadingtest","weather함수 끝");
+                            deg();
+                            Log.i("loadingtest","deg함수 끝");
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+
                     }
+
                 });
+
+
                 t.start();
+
 
 
             }
@@ -111,12 +122,13 @@ public class MainActivity extends AppCompatActivity {
         });
         //새로고침 버튼 눌렀을 때↑
 
-        btndeg.setOnClickListener(new View.OnClickListener() {
+        //온도 꺼내오기 버튼 눌렀을 때 ↓
+        /*btndeg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sqlDB = myHelper.getReadableDatabase();
                 Cursor cursor;
-                SimpleDateFormat time = new SimpleDateFormat("HHmm");
+                SimpleDateFormat time = new SimpleDateFormat("HHmm");*/
 
                 /*
                 시간 알고리즘
@@ -132,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
                  */
 
                 //if(Integer.parseInt(time.format(date))<1700){
-                    cursor = sqlDB.rawQuery("Select * from village where fcstDate=baseDate and fcstTime=1500 and category='T3H';", null);
+                    /*cursor = sqlDB.rawQuery("Select * from village where fcstDate=baseDate and fcstTime=1500 and category='T3H';", null);
                     String fvale = "";
 
                     while (cursor.moveToNext()){
@@ -164,12 +176,12 @@ public class MainActivity extends AppCompatActivity {
 
                     cursor.close();
                     sqlDB.close();
-
+                //}
                 }
 
-            //}
-        });
 
+        });*/
+        //온도 꺼내오기 버튼 눌렀을 때 ↑
 
         //광고↓
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
@@ -178,18 +190,185 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
         //광고↑
 
 
 
 
     }
+
+    //온도 꺼내오기 함수 ↓
+            public void deg() {
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                SimpleDateFormat time = new SimpleDateFormat("HHmm");
+
+                /*
+                시간 알고리즘
+                Base_time : 200, 500, 800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+                현재시간<300 -> where fcstDate and 0000=fcstTime
+                현재시간<600 -> where 300=fcstTime
+                현재시간<900 -> where 600=fcstTime
+                현재시간<1200 -> where 900=fcstTime
+                현재시간<1500 -> where 1200=fcstTime
+                현재시간<1800 -> where 1500=fcstTime
+                현재시간<2100 -> where 1800=fcstTime
+                현재시간<0000 -> where fcstDate-1 and 2100=fcstTime
+                 */
+            Log.i("timetest1",time.format(date));
+                if(Integer.parseInt(time.format(date))<1800){
+                    Log.i("timetest1","1");
+                    cursor = sqlDB.rawQuery("Select * from village where fcstDate=baseDate and fcstTime=1500 and category='T3H';", null);
+                    String fvale = "";
+
+                    while (cursor.moveToNext()){
+                        fvale += cursor.getString(5);
+                        Log.i("fvaletest",fvale);
+                    }
+
+
+                SimpleDateFormat time1 = new SimpleDateFormat("MM.dd. HH:mm");
+                    tvupdate.setText("최근 업데시트 시간 : "+(time1.format(date1)));
+                    String upcolor = "#000000";
+                    tvupdate.setTextColor(Color.parseColor(upcolor));
+                    if(Integer.parseInt(fvale)<0) {
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("영하에요!");
+                        tvcloth.setText("두꺼운 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<9){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("추워요!");
+                        tvcloth.setText("따뜻한 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<15){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("쌀쌀해요!");
+                        tvcloth.setText("가벼운 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<20){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("선선해요!");
+                    }else if(Integer.parseInt(fvale)<25){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("따뜻해요!");
+                    }else if(Integer.parseInt(fvale)<30){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("약간 더워요!");
+                    }else if(30<=Integer.parseInt(fvale)){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("더워요!");
+                    }
+                    else{
+                        tvcom1.setText("무언가 오류!");
+                    }
+
+                    cursor.close();
+                    sqlDB.close();
+                }else if(Integer.parseInt(time.format(date))<2100){
+                    Log.i("timetest1","2");
+                    cursor = sqlDB.rawQuery("Select * from village where fcstDate=baseDate and fcstTime=1800 and category='T3H';", null);
+                    String fvale = "";
+
+                    while (cursor.moveToNext()){
+                        fvale += cursor.getString(5);
+                        Log.i("fvaletest",fvale);
+                    }
+
+
+                    SimpleDateFormat time1 = new SimpleDateFormat("MM.dd. HH:mm");
+                    tvupdate.setText("최근 업데시트 시간 : "+(time1.format(date1)));
+                    String upcolor = "#000000";
+                    tvupdate.setTextColor(Color.parseColor(upcolor));
+                    if(Integer.parseInt(fvale)<0) {
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("영하에요!");
+                        tvcloth.setText("두꺼운 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<9){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("추워요!");
+                        tvcloth.setText("따뜻한 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<15){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("쌀쌀해요!");
+                        tvcloth.setText("가벼운 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<20){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("선선해요!");
+                    }else if(Integer.parseInt(fvale)<25){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("따뜻해요!");
+                    }else if(Integer.parseInt(fvale)<30){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("약간 더워요!");
+                    }else if(30<=Integer.parseInt(fvale)){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("더워요!");
+                    }
+                    else{
+                        tvcom1.setText("무언가 오류!");
+                    }
+
+                    cursor.close();
+                    sqlDB.close();
+                }else if(Integer.parseInt(time.format(date))<2400){
+                    Log.i("timetest1","3");
+                    cursor = sqlDB.rawQuery("Select * from village where fcstDate=baseDate and fcstTime=2100 and category='T3H';", null);
+                    String fvale = "";
+
+                    while (cursor.moveToNext()){
+                        fvale += cursor.getString(5);
+                        Log.i("fvaletest",fvale);
+                    }
+
+
+                    SimpleDateFormat time1 = new SimpleDateFormat("MM.dd. HH:mm");
+                    tvupdate.setText("최근 업데시트 시간 : "+(time1.format(date1)));
+                    String upcolor = "#000000";
+                    tvupdate.setTextColor(Color.parseColor(upcolor));
+                    if(Integer.parseInt(fvale)<0) {
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("영하에요!");
+                        tvcloth.setText("두꺼운 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<9){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("추워요!");
+                        tvcloth.setText("따뜻한 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<15){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("쌀쌀해요!");
+                        tvcloth.setText("가벼운 외투가 필요해요");
+                    }else if(Integer.parseInt(fvale)<20){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("선선해요!");
+                    }else if(Integer.parseInt(fvale)<25){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("따뜻해요!");
+                    }else if(Integer.parseInt(fvale)<30){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("약간 더워요!");
+                    }else if(30<=Integer.parseInt(fvale)){
+                        tvdeg.setText(fvale+"℃");
+                        tvcom1.setText("더워요!");
+                    }
+                    else{
+                        tvcom1.setText("무언가 오류!");
+                    }
+
+                    cursor.close();
+                    sqlDB.close();
+                }
+                }
+
+
+
+    //온도 꺼내오기 함수 ↑
+
     public void weather() throws IOException {
         SimpleDateFormat fourteen_format = new SimpleDateFormat("yyyyMMdd");
         Log.i("timetest",Integer.parseInt(fourteen_format.format(date))+"");
         for(int i=1;i<150;i++) {
             StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst"); /*URL*/
-            urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=j3VbMNWCQKFxaQ4nRw2%2BX4%2BGMdddZQerxp6RIpyU78DGfBiVwnHli4vXdpIn9ldST%2FXHZ6ahHUw16ieG7ynh7g%3D%3D"); /*Service Key*/
             //urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("-", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
             urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(i+"", "UTF-8")); /*페이지번호*/
             urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*한 페이지 결과 수*/
@@ -298,6 +477,22 @@ public class MainActivity extends AppCompatActivity {
             onCreate(db);
         }
     }
+    public class myDBHelper2 extends SQLiteOpenHelper {
+        public myDBHelper2(Context context){
+            super(context, "weather", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("Create table vlist(baseDate Integer , baseTime Integer, category char, fcstDate Integer, fcstTime Integer, fcstValue char, nx Integer, ny Integer);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("Drop table if exists vlist");
+            onCreate(db);
+        }
+    }
     //DB 생성 클래스↑
 
     //intent로 지역 들고옴 ↓
@@ -310,16 +505,32 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
             String sendText = data.getExtras().getString("result");
+
             Log.i("resulttest",sendText);
             tvloc.setText(sendText+"");
-            String[] locarray = sendText.split("\\s");
-            Log.i("splittest",locarray[1]+"");
-            String level1 = locarray[1];
-            String level2 = locarray[2];
-            String level3 = locarray[3];
-            Cursor cursor2;
-            sqlDB = myHelper.getWritableDatabase();
-            cursor2 = sqlDB.rawQuery("Select nx,ny from vlist where level1='"+level1+"' and level2='"+level2+"' and level3='"+level3+"';", null);
+            String[] locarray = new String[4];
+            locarray[0]="";
+            locarray[1]="";
+            locarray[2]="";
+            locarray[3]="";
+            locarray = sendText.split("\\s");
+            Log.i("splittest",locarray.length+"");
+            /*
+            Log.i("splittest",locarray[0]+"0");
+            Log.i("splittest",locarray[1]+"1");
+            Log.i("splittest",locarray[2]+"2");
+            Log.i("splittest",locarray[3]+"3");*/
+
+
+
+
+            if (locarray.length==4) {
+
+                Log.i("leveltest", "1번");
+                Cursor cursor2;
+                sqlDB = myHelper.getWritableDatabase();
+                cursor2 = sqlDB.rawQuery("Select nx,ny from vlist where level1='" + locarray[1] + "' and level2='" + locarray[2] + "' and level3='" + locarray[3] + "';", null);
+
 
             while(cursor2.moveToNext()) {
                 locnx = cursor2.getString(0);
@@ -329,6 +540,46 @@ public class MainActivity extends AppCompatActivity {
             }
             cursor2.close();
             sqlDB.close();
+            tvupdate.setText("새로고침을 누르세요 ↑");
+            tvdeg.setText("");
+            tvcloth.setText("");
+            }else if (locarray.length==3) {
+
+                Log.i("leveltest", "2번");
+                Cursor cursor2;
+                sqlDB = myHelper.getWritableDatabase();
+                cursor2 = sqlDB.rawQuery("Select nx,ny from vlist where level1='" + locarray[1] + "' and level2='" + locarray[2] + "' and level3 is null;", null);
+
+
+                while(cursor2.moveToNext()) {
+                    locnx = cursor2.getString(0);
+                    locny = cursor2.getString(1);
+                    Log.i("nxtest", locnx);
+                    Log.i("nxtest",locny);
+                }
+                cursor2.close();
+                sqlDB.close();
+                tvcom1.setText("새로고침을 누르세요");
+            }else if (locarray.length==2) {
+
+                Log.i("leveltest", "3번");
+                Cursor cursor2;
+                sqlDB = myHelper.getWritableDatabase();
+                cursor2 = sqlDB.rawQuery("Select nx,ny from vlist where level1='" + locarray[1] + "' and level2 is null and level3 is null;", null);
+
+
+                while(cursor2.moveToNext()) {
+                    locnx = cursor2.getString(0);
+                    locny = cursor2.getString(1);
+                    Log.i("nxtest", locnx);
+                    Log.i("nxtest",locny);
+                }
+                cursor2.close();
+                sqlDB.close();
+                tvcom1.setText("새로고침을 누르세요");
+            }else{
+                Log.i("leveltest","삐빅 오류~~");
+            }
         }
     }
     //intent로 지역 들고옴 ↑
